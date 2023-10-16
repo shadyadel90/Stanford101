216a5107467b5dee690f9f644ad0d33c8387af1c
shadyadel90 <31343078+shadyadel90@users.noreply.github.com>
2023-10-16 13:12:04 +0300

    initial commit

---
+++ First/First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Shady Adel on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
